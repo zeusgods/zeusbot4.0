b3bb5d9a3ec757033ba637567a52a3ae8625ac1a
zeusgods <79343597+zeusgods@users.noreply.github.com>
2021-02-20 19:16:05 -0200

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,34 @@
+const help = (prefix) => {
+ return `>
+         ═════✪〘 STICKER 〙✪═════
+comando : *${prefix}sticker* or *${prefix}stiker*
+comando : *${prefix}sticker nobg* or *${prefix}stiker nobg*
+comando : *${prefix}toimg*
+comando : *${prefix}tsticker* or *${prefix}tstiker*
+         ═════✪〘 MEME 〙✪═════
+comando : *${prefix}meme*
+comando : *${prefix}memeindo*
+         ═════✪〘 OUTROS 〙✪═════
+comando : *${prefix}gtts*
+comando : *${prefix}loli*
+command : *${prefix}nsfwloli*
+comando : *${prefix}url2img*
+comando : *${prefix}simi*
+comando : *${prefix}ocr*
+comando : *${prefix}wait*
+comando : *${prefix}setprefix*
+         ═════✪〘 GRUPOS 〙✪═════<
+comando : *${prefix}add*
+comando : *${prefix}kick*
+comando : *${prefix}promote*
+comando : *${prefix}demote*
+comando : *${prefix}linkgroup*
+comando : *${prefix}leave*
+comando : *${prefix}tagall*
+comando : *${prefix}simih*
+comando : *${prefix}dono*
+comando : *${prefix}nulia*
+        ═════✪〘 GRUPO 〙✪═════`
+}
+
+exports.help = help
